fa7afd099b1d8409c1300e9c096b17afb1bdb265
Carl-Y <3387640174@qq.com>
2024-04-22 16:47:05 +0800

    Update

---
+++ LinearRegressionNeuralNetwork_Simple.py
@@ -0,0 +1,27 @@
+import random
+import torch
+from d2l import torch as d2l
+
+
+def LRNN_S():
+    print('-----生成数据集-----')
+
+
+    print('-----读取数据集-----')
+
+
+    print('-----初始化模型参数-----')
+
+
+    print('-----训练-----')
+
+
+    return
+
+
+if __name__ == '__main__':
+    print(torch.backends.mps.is_available())
+    print(torch.backends.mps.is_built())
+
+    print('这是线性回归神经网络的简洁实现')
+    LRNN_S()
